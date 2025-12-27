31b6ea6980b00e5380fd6befe432f445a58f0219
Savchik <savchik@gmail.com>
2025-12-27 16:56:52 +0500

    add real vercel host...

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3002;
   app.enableCors({
-    origin: ["http://localhost:3000", "family-storage-cctn.vercel.app"],
+    origin: ["http://localhost:3000", "https://family-storage-cctn.vercel.app"],
     credentials: true,
   })
   await app.listen(port);
